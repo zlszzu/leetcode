27a8b3695c33077b076d3560d535c2817edd6841
zhaols <zhaols@zhaolsdeMacBook-Pro.local>
2021-06-16 00:25:12 +0800

    twoSum

---
+++ 0001-twoSum.c
@@ -0,0 +1,23 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    for (int i = 0; i < numsSize; ++i) {
+        int other = target - nums[i];
+        for (int j = i + 1; j < numsSize; ++j) {
+            if (other == nums[j]) {
+                int *result = (int *)malloc(2 * sizeof(int));
+                if (result == NULL) {
+                    printf("malloc failed\n");
+                    return NULL;
+                }
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
